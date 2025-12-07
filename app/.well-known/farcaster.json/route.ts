6a8867e3dec26f4b3339287696191dbdf44cfcff
Shantanu Swami <shantanuswami@Shantanus-MacBook-Air.local>
2025-12-07 19:08:29 +0530

    farcaster

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -4,11 +4,11 @@ import { APP_URL } from "../../../lib/constants";
 export async function GET() {
   const farcasterConfig = {
     // TODO: Add your own account association from Warpcast Developer Portal
-    // accountAssociation: {
-    //   header: "YOUR_HEADER_HERE",
-    //   payload: "YOUR_PAYLOAD_HERE",
-    //   signature: "YOUR_SIGNATURE_HERE"
-    // },
+  "accountAssociation": {
+    "header": "eyJmaWQiOjE1NzA0OTQsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHg4ZTg3ODliM0ZjRDA3NkFBOTEzOGQwRThmNWY4ZTAwNkU5ODE0OTQ1In0",
+    "payload": "eyJkb21haW4iOiJiYXNlLWZhcmNhc3Rlci1pb3RhLnZlcmNlbC5hcHAifQ",
+    "signature": "KG/XdECJgEuK+EB6CQhxCMfBLHBXv64CNXFroW73SaIvaRVbkWPuZ02mm/OiqbnqtgFmxQsSp5U1mhpfgqO3TRs="
+  },
     frame: {
       version: "1",
       name: "Battle Arena - NFT Fighter",
